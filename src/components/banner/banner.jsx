29967bed40b6262b63fdd74e26047e9e4628db96
abdialimovtemur <abdialimovtemur@gmail.com>
2024-10-03 11:23:28 +0500

    Commit title

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import hero from "../../assets/hero.svg"
+import style from "./style.module.scss"
 
 export const Banner = () => {
-    return <div>Banner</div>
+    return (
+        <div className={style.banner}>
+            <img src={hero} alt="" />
+        </div>
+    )
 };
